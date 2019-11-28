78085eb313099024595a8770630e6b93bdc5f117
Abdelrahmen Ayman <abdelrahmenayman7@gmail.com>
2019-11-29 01:53:33 +0200

    Replace permissions File

--- authentication/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsNotAuthenticated(BasePermission):
-
-    def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return False
-        return True
